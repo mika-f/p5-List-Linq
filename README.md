a54e7a57090be05082334c05809bef8939e0330a
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2020-12-08 09:59:14 +0900

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+List::Linq
+
+# SYNOPSIS
+
+    use List::Linq;
+
+# DESCRIPTION
+
+A powerful language integrated query (LINQ) library for Perl5.
+
+# LICENSE
+
+Copyright (C) Mikazuki Fuyuno.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Mikazuki Fuyuno &lt;mikazuki\_fuyuno@outlook.com>
---
+++ lib/List/Linq.pm
@@ -0,0 +1,39 @@
+package List::Linq;
+use 5.030;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+List::Linq - It's new $module
+
+=head1 SYNOPSIS
+
+    use List::Linq;
+
+=head1 DESCRIPTION
+
+List::Linq is ...
+
+=head1 LICENSE
+
+Copyright (C) Mikazuki Fuyuno.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Mikazuki Fuyuno E<lt>mikazuki_fuyuno@outlook.comE<gt>
+
+=cut
+
